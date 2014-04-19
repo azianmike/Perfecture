ebcf4c89e3d93b9e810b1986acbb94cab712413e
michaelluo <michaeluo@gmail.com>
2014-04-19 01:01:04 -0500

    Brand new app!

---
+++ src/asian/mike/perfecture/MainScreen.java
@@ -0,0 +1,76 @@
+package asian.mike.perfecture;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images.Media;
+import android.app.Activity;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.ImageView;
+
+public class MainScreen extends Activity {
+	
+	public static final int GALLERY_CODE = 322;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_screen);
+		
+	    String[] projection = {MediaStore.Images.Media.DATA};
+
+		ContextWrapper context = (ContextWrapper) getApplicationContext();
+		final Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+	            projection,
+	            null,
+	            null,
+	            null);
+
+	    ArrayList<String> result = new ArrayList<String>(cursor.getCount());
+
+	    Log.i("cursor.getCount()) :", cursor.getCount() + "");
+
+	    if (cursor.moveToFirst()) {
+	        final int dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+	        do {
+	            final String data = cursor.getString(dataColumn);
+	            Log.i ("data :",data );
+	            result.add(data);
+	        } while (cursor.moveToNext());
+	    }
+	    cursor.close();
+	    
+	    ImageView firstImageView = (ImageView) findViewById(R.id.imageView1);
+	    Uri link = Uri.parse("file://"+result.get(0));
+	    Bitmap bitmap = null;
+	    try {
+			bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), link);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    firstImageView.setImageBitmap(bitmap);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_screen, menu);
+		return true;
+	}
+
+
+}
